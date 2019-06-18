ada6ae68d09ee147c2b1b6fe6f595e556355058f
Alastrim <nik_kalin@mail.ru>
2019-06-18 18:19:47 +0300

    ENH: Abr: Play with fonts

--- Abridged/build.sh
+++ Abridged/build.sh
@@ -1,9 +1,7 @@
 set -e
 
-pdflatex -halt-on-error example.tex
-pdflatex -halt-on-error example.tex
-pdflatex -halt-on-error abr.tex
-pdflatex -halt-on-error abr.tex
+pdflatex -file-line-error -halt-on-error abr.tex
+pdflatex -file-line-error -halt-on-error abr.tex
 
 rm *.log
 rm *.aux
