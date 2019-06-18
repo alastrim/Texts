c7a18c9eb247d5282d780a7b9d97a0cda48bb5f9
Alastrim <nik_kalin@mail.ru>
2019-06-18 17:02:32 +0300

    ENH: Abr: Stat stars for high stat scores

--- Abridged/build.sh
+++ Abridged/build.sh
@@ -1,7 +1,9 @@
-pdflatex example.tex
-pdflatex example.tex
-pdflatex abr.tex
-pdflatex abr.tex
+set -e
+
+pdflatex -halt-on-error example.tex
+pdflatex -halt-on-error example.tex
+pdflatex -halt-on-error abr.tex
+pdflatex -halt-on-error abr.tex
 
 rm *.log
 rm *.aux
